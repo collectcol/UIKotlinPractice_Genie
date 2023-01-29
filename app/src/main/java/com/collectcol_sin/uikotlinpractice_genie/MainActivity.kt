22c4f9518b168ba4a4e374b5e66183d94290db3d
collectcol <sinwookyoung15@gmail.com>
2023-01-29 19:14:15 +0900

    Initial commit

---
+++ app/src/main/java/com/collectcol_sin/uikotlinpractice_genie/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.collectcol_sin.uikotlinpractice_genie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
